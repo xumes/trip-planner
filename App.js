b3f8cdce9f008833c1ba16b81c07d51d5f080dec
Reginaldo Santos <rmsantos@br.ibm.com>
2018-12-12 00:07:09 -0200

    application bootstrap

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
