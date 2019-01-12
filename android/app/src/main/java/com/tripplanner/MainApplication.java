b5162c4af0cb7ccf993b0cf5bf64d5c2fa3bedd0
Reginaldo Santos <rmsantos@br.ibm.com>
2019-01-12 21:19:01 -0200

    activating Google Maps for Android

--- android/app/src/main/java/com/tripplanner/MainApplication.java
+++ android/app/src/main/java/com/tripplanner/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.airbnb.android.react.maps.MapsPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
