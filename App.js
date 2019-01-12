ae08ae719096f998ec2587f872b480ac4fe96862
Reginaldo Santos <rmsantos@br.ibm.com>
2019-01-12 00:34:35 -0200

    changes on the App in order to support the Map

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './src/screens/HomeScreen'
 import TripsScreen from './src/screens/TripsScreen'
 import TripScreen from './src/screens/TripScreen'
+import AddPointScreen from './src/screens/AddPointScreen'
+import AddTripScreen from './src/screens/AddTripScreen'
 
 const appNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Trips: TripsScreen,
-    Trip: TripScreen
+    Trip: TripScreen,
+    AddPoint: AddPointScreen,
+    AddTrip: AddTripScreen
   },
   {
     initialRouteName: 'Home'
